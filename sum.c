d2d0326c9fbee78f7a2452a1ed83ab55e492c49b
Sanskar Gupta <sg0184@srmist.edu.in>
2024-09-25 10:00:36 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -2,14 +2,12 @@
 #include <math.h>
 
 int main() {
-    int a; // defining the type
-    int b;
-    int c;
-    printf("enter number 1 - ");
-    scanf("%d", &a); // inputting the number
-    printf("enter number 2 -");
-    scanf("%d", &b);
-    c = a + b;
-    printf("the sum of both the numbers is = %d \n", c); // printing the answer
+    int num1, num2, sum // defining the type
+    printf("enter number 1 ");
+    scanf("%d", &num1); // inputting the number
+    printf("enter number 2 ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("the sum is = %d \n", sum); // printing the answer
     return 0;
 }
