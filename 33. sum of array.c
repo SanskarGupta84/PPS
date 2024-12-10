55f9f6b1c94a554007b34d2a68730ffd3c15a7dd
Sanskar Gupta <sg0184@srmist.edu.in>
2024-12-11 00:11:43 +0530

    Create 33. sum of array.c

---
+++ 33. sum of array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3};
+    printf("Sum: %d\n", sumArray(arr, 3));
+    return 0;
+}
