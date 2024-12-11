d8c79408bc3812c1744a86d2efacaf35710753d7
Sanskar Gupta <sg0184@srmist.edu.in>
2024-12-11 14:24:34 +0530

    Create 41. dictionary.py

---
+++ 41. dictionary.py
@@ -0,0 +1,9 @@
+# Create a dictionary
+person = {"name": "Alice", "age": 25}
+# Access a value
+print(person["name"]) 
+# Add a key-value pair
+person["city"] = "New York"
+# Remove a key-value pair
+del person["age"]
+print(person)
