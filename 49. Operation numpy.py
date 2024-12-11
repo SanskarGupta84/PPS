454b622abacc2026fe26c9c6fb654a08604a39cc
Sanskar Gupta <sg0184@srmist.edu.in>
2024-12-11 21:49:06 +0530

    Create 49. Operation numpy.py

---
+++ 49. Operation numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+
+print("Addition:", a + b)       # Addition
+print("Subtraction:", a - b)    # Subtraction
+print("Multiplication:", a * b) # Multiplication
+print("Division:", a / b)       # Division
